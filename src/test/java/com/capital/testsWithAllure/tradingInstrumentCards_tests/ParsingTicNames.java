c014ae1a7f75394eaf08c3d675ac59704a403393
Ivan Filimonov <001i@rambler.ru>
2023-03-27 11:16:27 +0500

    add new dataset in sharesTicName.csv

--- src/test/java/com/capital/testsWithAllure/tradingInstrumentCards_tests/ParsingTicNames.java
+++ src/test/java/com/capital/testsWithAllure/tradingInstrumentCards_tests/ParsingTicNames.java
@@ -37,38 +37,38 @@ public class ParsingTicNames extends MethodsWithAllure {
   public void createCSVFile() {
     ArrayList<String> languages = new ArrayList<>();
     languages.add("\"\""); // English
-//    languages.add("ar");   // Arab
-//    languages.add("id");   // Indonesian
-//    languages.add("bg");   // Bulgarian
-//    languages.add("cs");   // Czech
-//    languages.add("da");   // Denmark
+//    languages.add("ar/");   // Arab
+//    languages.add("id/");   // Indonesian
+//    languages.add("bg/");   // Bulgarian
+//    languages.add("cs/");   // Czech
+//    languages.add("da/");   // Denmark
 //
-//    languages.add("de");   // Germany
-//    languages.add("et");   // Estonian
-//    languages.add("el");   // Greek
-//    languages.add("es");   // Spanish
-//    languages.add("fr");   // French
-//    languages.add("hr");   // Croatia
+//    languages.add("de/");   // Germany
+//    languages.add("et/");   // Estonian
+//    languages.add("el/");   // Greek
+//    languages.add("es/");   // Spanish
+//    languages.add("fr/");   // French
+//    languages.add("hr/");   // Croatia
 //
-//    languages.add("it");   // Italian
-//    languages.add("lv");   // Latvian
-//    languages.add("lt");   // Estonian
-//    languages.add("hu");   // Hungarian
-//    languages.add("nl");   // Netherlands
-//    languages.add("pl");   // Polish
+//    languages.add("it/");   // Italian
+//    languages.add("lv/");   // Latvian
+//    languages.add("lt/");   // Estonian
+//    languages.add("hu/");   // Hungarian
+//    languages.add("nl/");   // Netherlands
+//    languages.add("pl/");   // Polish
 //
-//    languages.add("pt");   // Portuguese
-//    languages.add("ro");   // Romania
-//    languages.add("ru");   // Russian
-//    languages.add("sk");   // Slovak
-//    languages.add("sl");   // Slovenian
-//    languages.add("fi");   // Finnish
+//    languages.add("pt/");   // Portuguese
+//    languages.add("ro/");   // Romania
+//    languages.add("ru/");   // Russian
+//    languages.add("sk/");   // Slovak
+//    languages.add("sl/");   // Slovenian
+//    languages.add("fi/");   // Finnish
 //
-//    languages.add("sv");   // Swedish
-//    languages.add("vi");   // Vietnamese
-//    languages.add("th");   // Thai
-//    languages.add("zh");   // Simplified Chinese
-//    languages.add("cn");   // Traditional Chinese
+//    languages.add("sv/");   // Swedish
+//    languages.add("vi/");   // Vietnamese
+//    languages.add("th/");   // Thai
+//    languages.add("zh/");   // Simplified Chinese
+//    languages.add("cn/");   // Traditional Chinese
 
 
     ArrayList<String> licenses = new ArrayList<>();
@@ -80,11 +80,11 @@ public class ParsingTicNames extends MethodsWithAllure {
 //    licenses.add("FSA");     // Capital Com Stock and CFD Investing Ltd
 //    licenses.add("SCB");     // Capital Com Online Investments Ltd
 
-    CreateUrlForTests.createUrl(Locators.ADDRESS_CSV_FILE_FOREX, languages, Locators.ADDRESS_TIC_NAME_FOREX, licenses);
+//    CreateUrlForTests.createUrl(Locators.ADDRESS_CSV_FILE_FOREX, languages, Locators.ADDRESS_TIC_NAME_FOREX, licenses);
     CreateUrlForTests.createUrl(Locators.ADDRESS_CSV_FILE_INDICES, languages, Locators.ADDRESS_TIC_NAME_INDICES, licenses);
-    CreateUrlForTests.createUrl(Locators.ADDRESS_CSV_FILE_CRYPTOCURRENCIES, languages, Locators.ADDRESS_TIC_NAME_CRYPTOCURRENCIES, licenses);
-    CreateUrlForTests.createUrl(Locators.ADDRESS_CSV_FILE_COMMODITIES, languages, Locators.ADDRESS_TIC_NAME_COMMODITIES, licenses);
-    CreateUrlForTests.createUrl(Locators.ADDRESS_CSV_FILE_SHARES, languages, Locators.ADDRESS_TIC_NAME_SHARES, licenses);
+//    CreateUrlForTests.createUrl(Locators.ADDRESS_CSV_FILE_CRYPTOCURRENCIES, languages, Locators.ADDRESS_TIC_NAME_CRYPTOCURRENCIES, licenses);
+//    CreateUrlForTests.createUrl(Locators.ADDRESS_CSV_FILE_COMMODITIES, languages, Locators.ADDRESS_TIC_NAME_COMMODITIES, licenses);
+//    CreateUrlForTests.createUrl(Locators.ADDRESS_CSV_FILE_SHARES, languages, Locators.ADDRESS_TIC_NAME_SHARES, licenses);
   }
 
 }
